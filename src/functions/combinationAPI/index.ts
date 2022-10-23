e1588484432c79184b1002f8c457f4d3688b9666
Olusola Oseni <kenolusola@gmail.com>
2022-10-23 02:29:15 +0100

    handle request error

--- src/functions/combinationAPI/index.ts
+++ src/functions/combinationAPI/index.ts
@@ -4,75 +4,69 @@ import Axios from "axios";
 
 // lambda function
 export const handler = async (event: APIGatewayProxyEvent) => {
-  // API request ===> ourURL/gameDeals?currency=eur
+  try {
+    // API request ===> ourURL/gameDeals?currency=eur
 
-  const { queryStringParameters = {} } = event;
+    const { queryStringParameters = {} } = event;
 
-  const { currency } = queryStringParameters;
+    const { currency } = queryStringParameters;
 
-  if (!currency) {
+    if (!currency) {
+      return formatJSONResponse({
+        statusCode: 400,
+        data: {
+          message: "Missing currency query parameter",
+        },
+      });
+    }
+
+    const deals = await Axios.get(
+      "https://www.cheapshark.com/api/1.0/deals?upperPrice=15&pageSize=10"
+    );
+
+    // changing from one USD currency to the currency passed by the user
+    const currencyData = await Axios.get(
+      `https://cdn.jsdelivr.net/gh/fawazahmed0/currency-api@1/latest/currencies/usd/${currency}.json`
+    );
+
+    const currencyConversion = currencyData.data[currency];
+
+    const repricedDeals = deals.data.map((deal) => {
+      const {
+        title,
+        storeID,
+        gameID,
+        salesPrice,
+        normalPrice,
+        savings,
+        dealRating,
+        releaseDate,
+        steamRatingPercent,
+      } = deal;
+
+      return {
+        title,
+        storeID,
+        steamRatingPercent,
+        gameID,
+        dealRating,
+        salesPrice: salesPrice * currencyConversion,
+        normalPrice: normalPrice * currencyConversion,
+        savingsPercent: savings,
+        releaseDate: new Date(releaseDate * 1000).toDateString(),
+      };
+    });
+
     return formatJSONResponse({
-      statusCode: 400,
+      data: repricedDeals,
+    });
+  } catch (error) {
+    console.log(error);
+    return formatJSONResponse({
+      statusCode: 500,
       data: {
-        message: "Missing currency query parameter",
+        message: error.message,
       },
     });
   }
-
-  const deals = await Axios.get(
-    "https://www.cheapshark.com/api/1.0/deals?upperPrice=15&pageSize=10"
-  ).catch((error) => {
-    console.log(error);
-    return formatJSONResponse({
-      statusCode: 500,
-      data: {
-        message: error.message,
-      },
-    });
-  });
-
-  // changing from one USD currency to the currency passed by the user
-  const currencyData = await Axios.get(
-    `https://cdn.jsdelivr.net/gh/fawazahmed0/currency-api@1/latest/currencies/usd/${currency}.json`
-  ).catch((error) => {
-    console.log(error);
-    return formatJSONResponse({
-      statusCode: 500,
-      data: {
-        message: error.message,
-      },
-    });
-  });
-
-  const currencyConversion = currencyData.data[currency];
-
-  const repricedDeals = deals.data.map((deal) => {
-    const {
-      title,
-      storeID,
-      gameID,
-      salesPrice,
-      normalPrice,
-      savings,
-      dealRating,
-      releaseDate,
-      steamRatingPercent,
-    } = deal;
-
-    return {
-      title,
-      storeID,
-      steamRatingPercent,
-      gameID,
-      dealRating,
-      salesPrice: salesPrice * currencyConversion,
-      normalPrice: normalPrice * currencyConversion,
-      savingsPercent: savings,
-      releaseDate: new Date(releaseDate * 1000).toDateString(),
-    };
-  });
-
-  return formatJSONResponse({
-    data: repricedDeals,
-  });
 };
--- src/libs/apiGateway.ts
+++ src/libs/apiGateway.ts
@@ -1,12 +1,14 @@
 export const formatJSONResponse = ({
   statusCode = 200,
   data = {},
-}): { statusCode?: number; data?: any; headers: any } => {
+}: {
+  statusCode?: number;
+  data?: any;
+}) => {
   return {
     statusCode,
-    data: JSON.stringify(data),
+    body: JSON.stringify(data),
     headers: {
-      // any web url can make a request to our API
       "Access-Control-Allow-Origin": "*",
       "Access-Control-Allow-Credentials": true,
     },
